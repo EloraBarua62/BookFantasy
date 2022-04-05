23b00ef964d85e9333cae93b7c7090fdb8421eb9
EloraBarua <elorabarua62@gmail.com>
2022-04-05 12:21:33 +0600

    costom hook created and review's data loaded

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import useReview from '../../hooks/useReview';
 
 const Review = () => {
+    const [reviews, setReviews] = useReview();
     return (
         <div>
-            
+            <h1 className='text-4xl font-semibold'>Total reviews of users is : {reviews.length}</h1>
         </div>
     );
 };
---
+++ src/hooks/useReview.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+
+const useReview = () => {
+    const [reviews,setReviews] = useState([]);
+    useEffect(()=>{
+        fetch('user_review.json')
+        .then(response => response.json())
+        .then(data => setReviews(data))
+    },[reviews])
+    return [reviews, setReviews];
+}
+export default useReview;
